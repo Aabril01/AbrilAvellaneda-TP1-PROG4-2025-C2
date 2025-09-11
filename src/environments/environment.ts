9a7335a1ad41d8580795aeb0e70a29e999ac1ed6
ABRIL <001aabril@gmail.com>
2025-09-11 18:33:49 -0300

    sup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
-// src/environments/environment.ts
 export const environment = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://knuowpdxcagtspfglfth.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtudW93cGR4Y2FndHNwZmdsZnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MjMzNDYsImV4cCI6MjA3MzE5OTM0Nn0.OFndJ_Lsjhy0nzytTh7NSrzm-xpdyGY3SAj7HWt4mYQ'
 };
